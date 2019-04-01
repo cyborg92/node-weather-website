44a228509212f1bde0ac8d21878bc142e0b0d675
Rachit Handa <rachithanda93@yahoo.com>
2019-04-01 16:42:18 +0530

    Init Commit

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form');
+const search=document.querySelector('input');
+let locationElem=document.getElementById('location');
+let forecastElem=document.getElementById('forecast');
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    fetch(`http://localhost:3000/weather?address=${search.value}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                locationElem.innerText=data.error;
+                forecastElem.innerText='';
+            } else {
+                locationElem.innerText=data.location;
+                forecastElem.innerText=data.forecast;
+            }
+        });
+    })
+})
